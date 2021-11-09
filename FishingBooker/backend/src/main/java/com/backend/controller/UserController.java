2f4ba8bd7bd737ae0f924cf6ee7d39197c5ad654
Ana Gavrilovic <gavrilovic.ra65.2018@uns.ac.rs>
2021-11-09 18:38:25 +0100

    Dodatna podesavanja backenda

--- FishingBooker/backend/src/main/java/com/backend/controller/UserController.java
+++ FishingBooker/backend/src/main/java/com/backend/controller/UserController.java
@@ -1,10 +1,11 @@
-package com.backend.controllers;
+package com.backend.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequestMapping("/user")
-public class UserController {
+public class  UserController {
 
 }
