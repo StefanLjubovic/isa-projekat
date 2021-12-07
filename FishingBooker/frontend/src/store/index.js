df3aecaf3a5c417a5fd41d51fff86e75929b7433
StefanLjubovic <ljubovicstefan@gmail.com>
2021-12-07 14:53:48 +0100

    Save local storage data after refresh

--- FishingBooker/frontend/src/store/index.js
+++ FishingBooker/frontend/src/store/index.js
@@ -1,7 +1,10 @@
 import {createStore} from 'vuex'
 import token from './modules/token';
-
+import createPersistedState from 'vuex-persistedstate'
 export default createStore({
+    plugins: [createPersistedState({
+        storage: window.sessionStorage,
+    })],
     modules: {
         token
     }
