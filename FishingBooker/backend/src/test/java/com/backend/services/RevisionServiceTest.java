a51276c9682ee94c314583cfda4fc18ff7a92776
Ana Gavrilovic <gavrilovic.ra65.2018@uns.ac.rs>
2022-01-22 21:14:14 +0100

    fix: revision test fixed

--- FishingBooker/backend/src/test/java/com/backend/services/RevisionServiceTest.java
+++ FishingBooker/backend/src/test/java/com/backend/services/RevisionServiceTest.java
@@ -37,7 +37,7 @@ public class RevisionServiceTest {
         Revision revision2 = new Revision(DB_REVISION_CONTENT_2, DB_REVISION_MARK_2, DB_REVISION_IS_APPROVED_2, null);
         Revision revision3 = new Revision(DB_REVISION_CONTENT_3, DB_REVISION_MARK_3, DB_REVISION_IS_APPROVED_3, null);
 
-        when(revisionRepositoryMock.findAll()).thenReturn(Arrays.asList(revision1, revision2, revision3));
+        when(revisionRepositoryMock.findAllByOrderByIdAsc()).thenReturn(Arrays.asList(revision1, revision2, revision3));
 
         // 2. Akcija
         List<Revision> pendingRevisions = revisionService.getAllPendingRevisions();
@@ -47,7 +47,7 @@ public class RevisionServiceTest {
         assertFalse(pendingRevisions.get(0).getApproved());
         assertFalse(pendingRevisions.get(1).getApproved());
 
-        verify(revisionRepositoryMock, times(1)).findAll();
+        verify(revisionRepositoryMock, times(1)).findAllByOrderByIdAsc();
         verifyNoMoreInteractions(revisionRepositoryMock);
 
     }
